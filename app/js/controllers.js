804682ebc3901b48493f13f9d50abddd00d27457
Tom Hanlon <tomhanlon@gmail.com>
2015-10-06 20:00:37 -0500

    Update category name

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,7 +8,7 @@ portfolioApp.controller('PortfolioCtrl', function($scope) {
   $scope.projects = [
     { 'name': 'This Very Portfolio',
     'thumbnail_url': 'https://unsplash.it/230/184?random',
-    'technologies': ['angular', 'javascript'],
+    'categories': ['angular', 'javascript'],
     'description': 'Lucas ipsum dolor sit amet watto qui-gon c-3po antilles darth organa solo skywalker jade jinn. Fisto han jar yoda darth fett. Palpatine mara padmé moff anakin skywalker fett jabba. Hutt antilles alderaan grievous solo anakin gonk hutt. Darth kamino mon hutt antilles moff solo amidala darth. Thrawn sidious darth moff. Windu droid vader vader. Hutt ahsoka padmé solo ackbar mandalorians darth leia. Binks owen cade grievous droid. Skywalker moff darth jabba owen. Palpatine baba hutt boba jango ewok kenobi.',
     'live_link': 'http://www.google.com',
     'github_link': 'https://github.com/tomhanlon/portfolio',
@@ -16,7 +16,7 @@ portfolioApp.controller('PortfolioCtrl', function($scope) {
   },
   { 'name': 'iVote',
     'thumbnail_url': 'https://unsplash.it/230/184?random',
-    'technologies': ['ruby on rails', 'javascript', 'cdyne', 'redis'],
+    'categories': ['ruby on rails', 'javascript', 'cdyne', 'redis'],
     'description': 'Lucas ipsum dolor sit amet watto qui-gon c-3po antilles darth organa solo skywalker jade jinn. Fisto han jar yoda darth fett. Palpatine mara padmé moff anakin skywalker fett jabba. Hutt antilles alderaan grievous solo anakin gonk hutt. Darth kamino mon hutt antilles moff solo amidala darth. Thrawn sidious darth moff. Windu droid vader vader. Hutt ahsoka padmé solo ackbar mandalorians darth leia. Binks owen cade grievous droid. Skywalker moff darth jabba owen. Palpatine baba hutt boba jango ewok kenobi.',
     'live_link': 'http://www.google.com',
     'github_link': 'www.constituentexts.com',
@@ -24,7 +24,7 @@ portfolioApp.controller('PortfolioCtrl', function($scope) {
   },
   { 'name': 'Carewell',
     'thumbnail_url': 'https://unsplash.it/230/184?random',
-    'technologies': ['ruby on rails'],
+    'categories': ['ruby on rails'],
     'description': 'Lucas ipsum dolor sit amet watto qui-gon c-3po antilles darth organa solo skywalker jade jinn. Fisto han jar yoda darth fett. Palpatine mara padmé moff anakin skywalker fett jabba. Hutt antilles alderaan grievous solo anakin gonk hutt. Darth kamino mon hutt antilles moff solo amidala darth. Thrawn sidious darth moff. Windu droid vader vader. Hutt ahsoka padmé solo ackbar mandalorians darth leia. Binks owen cade grievous droid. Skywalker moff darth jabba owen. Palpatine baba hutt boba jango ewok kenobi.',
     'live_link': 'http://www.google.com',
     'github_link': 'https://github.com/RollWave07/CareWell',
@@ -32,7 +32,7 @@ portfolioApp.controller('PortfolioCtrl', function($scope) {
   },
   { 'name': 'Weatherpane',
     'thumbnail_url': 'https://unsplash.it/230/184?random',
-    'technologies': ['geolocation', 'api'],
+    'categories': ['geolocation', 'api'],
     'description': 'Lucas ipsum dolor sit amet watto qui-gon c-3po antilles darth organa solo skywalker jade jinn. Fisto han jar yoda darth fett. Palpatine mara padmé moff anakin skywalker fett jabba. Hutt antilles alderaan grievous solo anakin gonk hutt. Darth kamino mon hutt antilles moff solo amidala darth. Thrawn sidious darth moff. Windu droid vader vader. Hutt ahsoka padmé solo ackbar mandalorians darth leia. Binks owen cade grievous droid. Skywalker moff darth jabba owen. Palpatine baba hutt boba jango ewok kenobi.',
     'live_link': 'http://www.google.com',
     'github_link': 'https://github.com/tejas-rawal/WeatherPane_v3',
@@ -40,7 +40,7 @@ portfolioApp.controller('PortfolioCtrl', function($scope) {
   },
   { 'name': 'Babyloader',
     'thumbnail_url': 'https://unsplash.it/230/184?random',
-    'technologies': ['html5', 'javascript'],
+    'categories': ['html5', 'javascript'],
     'description': 'Lucas ipsum dolor sit amet watto qui-gon c-3po antilles darth organa solo skywalker jade jinn. Fisto han jar yoda darth fett. Palpatine mara padmé moff anakin skywalker fett jabba. Hutt antilles alderaan grievous solo anakin gonk hutt. Darth kamino mon hutt antilles moff solo amidala darth. Thrawn sidious darth moff. Windu droid vader vader. Hutt ahsoka padmé solo ackbar mandalorians darth leia. Binks owen cade grievous droid. Skywalker moff darth jabba owen. Palpatine baba hutt boba jango ewok kenobi.',
     'live_link': 'http://www.google.com',
     'github_link': 'https://github.com/tomhanlon/babyloader',
@@ -48,7 +48,7 @@ portfolioApp.controller('PortfolioCtrl', function($scope) {
   },
   { 'name': 'Brisket Battle',
     'thumbnail_url': 'https://unsplash.it/230/184?random',
-    'technologies': ['ruby on rails'],
+    'categories': ['ruby on rails'],
     'description': 'Lucas ipsum dolor sit amet watto qui-gon c-3po antilles darth organa solo skywalker jade jinn. Fisto han jar yoda darth fett. Palpatine mara padmé moff anakin skywalker fett jabba. Hutt antilles alderaan grievous solo anakin gonk hutt. Darth kamino mon hutt antilles moff solo amidala darth. Thrawn sidious darth moff. Windu droid vader vader. Hutt ahsoka padmé solo ackbar mandalorians darth leia. Binks owen cade grievous droid. Skywalker moff darth jabba owen. Palpatine baba hutt boba jango ewok kenobi.',
     'live_link': 'http://www.google.com',
     'github_link': 'https://github.com/tomhanlon/bbq-battle',
@@ -56,17 +56,17 @@ portfolioApp.controller('PortfolioCtrl', function($scope) {
   },
   { 'name': 'Time Jump',
     'thumbnail_url': 'https://unsplash.it/230/184?random',
-    'technologies': ['iOS'],
+    'categories': ['iOS'],
     'description': 'Lucas ipsum dolor sit amet watto qui-gon c-3po antilles darth organa solo skywalker jade jinn. Fisto han jar yoda darth fett. Palpatine mara padmé moff anakin skywalker fett jabba. Hutt antilles alderaan grievous solo anakin gonk hutt. Darth kamino mon hutt antilles moff solo amidala darth. Thrawn sidious darth moff. Windu droid vader vader. Hutt ahsoka padmé solo ackbar mandalorians darth leia. Binks owen cade grievous droid. Skywalker moff darth jabba owen. Palpatine baba hutt boba jango ewok kenobi.',
     'image_url': 'http://www.placecage.com/835/318?random'
   }
   ];
   
-  $scope.technologies = [];
+  $scope.categories = [];
     angular.forEach($scope.projects, function(project){
-    angular.forEach(project.technologies, function(tech){
-      if ($scope.technologies.indexOf(tech) === -1) {
-        $scope.technologies.push(tech);
+    angular.forEach(project.categories, function(tech){
+      if ($scope.categories.indexOf(tech) === -1) {
+        $scope.categories.push(tech);
       }
     });
   });
